c13a35893dbed3e6d1bb0996fc0f0a5f007d027e
knj4484 <knj4484@gmail.com>
2019-09-24 08:12:03 +0900

    Refactor find_translations.rb

--- lib/helpers/find_translations.rb
+++ lib/helpers/find_translations.rb
@@ -1,22 +1,22 @@
 CONTENT_TOP_DIRS = %w(blog community docs download governance index webtools assets)
 
 def find_translations(item, items)
-  top_dir = item.path.split('/')[1]
-  language_code = (CONTENT_TOP_DIRS.include?(top_dir) or top_dir.nil?) ? 'en' : top_dir
-  canonical_path = language_code == 'en' ? item.path : item.path[3..-1]
-  translation_items = items.find_all {|i| get_language_code(i) != language_code and get_canonical_path(i) == canonical_path }
-  translations = translation_items.map {|i| { :path => i.identifier.to_s, :language_code => get_language_code(i) } }
-  language_code == 'en' ? translations : translations << { :path => canonical_path, :language_code => 'en' }
+  language_code = language_code(item)
+  canonic_path = canonical_path(item)
+  translation_items = items.find_all {|i| language_code(i) != language_code and canonical_path(i) == canonic_path }
+  translations = translation_items.map {|i| { :path => i.identifier.to_s, :language_code => language_code(i) } }
 end
 
 def langulage_path_prefix(item)
-  top_dir = item.path.split('/')[1]
+  top_dir = item.identifier.to_s.split('/')[1]
   language_code = (CONTENT_TOP_DIRS.include?(top_dir) or top_dir.nil?) ? '' : '/' + top_dir
 end
-def get_language_code(item)
-  item.identifier.to_s[1..2]
+def language_code(item)
+  top_dir = item.identifier.to_s.split('/')[1]
+  language_code = (CONTENT_TOP_DIRS.include?(top_dir) or top_dir.nil?) ? 'en' : top_dir
 end
 
-def get_canonical_path(item)
-  item.identifier.to_s[3..-1]
+def canonical_path(item)
+  prefix = langulage_path_prefix(item)
+  item.identifier.to_s[prefix.length..-1]
 end
